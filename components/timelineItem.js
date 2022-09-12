4a6ba9f46ef61066e2f8027760d54e5d3c154e7f
Aviv Faraj <avivfaraj@Avivs-iMac.local>
2022-09-12 14:42:20 -0400

    added timeline component

---
+++ components/timelineItem.js
@@ -0,0 +1,27 @@
+/* Thanks to Borges53 for the Timeline code:
+https://github.com/Borges53/Timeline/blob/main/components/Timeline/item.js
+*/
+
+import Link from 'next/link'
+import React from "react";
+import Styles from '../styles/timeline.module.css'
+
+
+export default function item({data}){
+  const {title, description, start_date } = data;
+  return(
+      <div className={Styles['container-item']}>
+        <div className={Styles['container-content-item']}>
+          <div className={Styles['container-content-header']}>
+            <span className={`${Styles["container-content-header-time"]}`}>
+              {title}
+              <div className={Styles['container-content-header-background']}>
+                {start_date}
+              </div>
+            </span>
+             <span className={Styles["container-content-desc-item"]}>{description}</span>
+          </div>
+        </div>
+      </div>
+    );
+}
---
+++ vars/about_timeline.js
@@ -0,0 +1,45 @@
+const steps =[
+  {
+    title: "Data Analyst Intern at USAA",
+    description: "Developed ML classification model during a 10-week internship at USAA, San Antonio.",
+    start_date: "Jun 2022",
+  },
+  {
+    title: "Upsilon Pi Epsilon Induction",
+    description: "Inducted as a member of Drexel Chapter of UPE, the international society for information and computing disciplines",
+    start_date: "May 2022",
+  },
+  {
+    title: "Drexel University",
+    description: "Enrolled in Master of Science in Data Science",
+    start_date: "Mar 2021",
+  },
+  {
+    title: "Graduated Electrical Engineering",
+    description: "Bachelor of Science in Electrical and Electronics Engineering with Magna Cum Laude.",
+    start_date: "Aug 2020",
+  },
+  {
+    title: "Graduated Physics",
+    description: "Bachelor of Science in Physics with Summa Cum Laude",
+    start_date: "Aug 2019",
+  },
+  {
+    title: "Dean's List - Physics",
+    description: "", 
+    start_date: "Apr 2019",
+  },
+  {
+    title: "Dean's List - Physics",
+    description: "",
+    start_date: "Apr 2018",
+  },
+  {
+    title: "Enrolled in Dual Bachelor Degree at Ariel University",
+    description: "Dual Bachelor in Physics & Electrical Engineering, Ariel University",
+    start_date: "Oct 2016",
+  }
+
+];
+
+export default steps;
