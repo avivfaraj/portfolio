5c12c587552ecf33cd09f0220a9bfb1de7c777ac
avivfaraj <avivfaraj4@gmail.com>
2023-11-01 15:06:22 -0400

    Add DS diploma

--- pages/honors-and-awards.js
+++ pages/honors-and-awards.js
@@ -8,30 +8,35 @@ function Honors() {
     <div className={Styles.main}>
       <h1>Honors & Awards</h1>
       <div className={Styles.horizontalCertificate}>
-        <h3>Honor Society - Upsilon Pi Epsilon</h3>
+        <h3>Master of Science in Data Science</h3>
         <LinkedImage {...HonorsArray[0]} />
+      </div>
+
+      <div className={Styles.horizontalCertificate}>
+        <h3>Honor Society - Upsilon Pi Epsilon</h3>
+        <LinkedImage {...HonorsArray[1]} />
       </div>
 
       <div className={Styles.verticalCertificate}>
         <h3>Magna Cum Laude - Electrical Engineering 2020</h3>
-        <LinkedImage {...HonorsArray[1]} />
         <LinkedImage {...HonorsArray[2]} />
+        <LinkedImage {...HonorsArray[3]} />
       </div>
 
       <div className={Styles.verticalCertificate}>
         <h3>Summa Cum Laude - Physics 2019</h3>
-        <LinkedImage {...HonorsArray[3]} />
         <LinkedImage {...HonorsArray[4]} />
-      </div>
-
-      <div className={Styles.verticalCertificate}>
-        <h3>Dean's List 2019</h3>
         <LinkedImage {...HonorsArray[5]} />
       </div>
 
       <div className={Styles.verticalCertificate}>
-        <h3>Dean's List 2018</h3>
+        <h3>Dean's List 2019</h3>
         <LinkedImage {...HonorsArray[6]} />
+      </div>
+
+      <div className={Styles.verticalCertificate}>
+        <h3>Dean's List 2018</h3>
+        <LinkedImage {...HonorsArray[7]} />
       </div>
 
       <div className={Styles.gap} />
--- vars/honors.js
+++ vars/honors.js
@@ -1,47 +1,52 @@
-const honors =[
+const honors = [
+  {
+    src: "/images/honors_awards/DS_Diploma.png",
+    height: 300,
+    width: 400,
+    alt: "Data Science Diploma",
+  },
   {
     src: "/images/honors_awards/UPE.jpeg",
     height: 300,
     width: 400,
-    alt: "Upsilon Pi Epsilon (UPE) Certificate"
+    alt: "Upsilon Pi Epsilon (UPE) Certificate",
   },
   {
     src: "/images/honors_awards/EE_diploma_english.jpeg",
     height: 350,
     width: 250,
-    alt: "Electrical Engineering Diploma - English"
+    alt: "Electrical Engineering Diploma - English",
   },
   {
     src: "/images/honors_awards/EE_diploma_hebrew.jpeg",
     height: 350,
     width: 250,
-    alt: "Electrical Engineering Diploma - Hebrew"
+    alt: "Electrical Engineering Diploma - Hebrew",
   },
   {
     src: "/images/honors_awards/Physics_diploma_english.jpeg",
     height: 350,
     width: 250,
-    alt: "Physics Diploma - English"
+    alt: "Physics Diploma - English",
   },
   {
     src: "/images/honors_awards/Physics_diploma_hebrew.jpeg",
     height: 350,
     width: 250,
-    alt: "Physics Diploma - Hebrew"
+    alt: "Physics Diploma - Hebrew",
   },
   {
     src: "/images/honors_awards/Dean_List_2019.jpeg",
     height: 350,
     width: 250,
-    alt: "Dean's List 2019 - Hebrew"
+    alt: "Dean's List 2019 - Hebrew",
   },
   {
     src: "/images/honors_awards/Dean_List_2018.jpeg",
     height: 350,
     width: 250,
-    alt: "Dean's List 2018 - Hebrew"
+    alt: "Dean's List 2018 - Hebrew",
   },
+];
 
-]
-
-export default honors
+export default honors;
