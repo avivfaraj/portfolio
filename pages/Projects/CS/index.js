c27c322380c8aaeadc2b8d02ebb5ed4c7ddcd0a1
avivfaraj <avivfaraj4@gmail.com>
2024-03-04 20:53:48 -0500

    Add revalidate to refresh page and show cards in different order

--- pages/Projects/CS/index.js
+++ pages/Projects/CS/index.js
@@ -25,6 +25,7 @@ export function getStaticProps() {
     props: {
       projects: randomizeProjects,
     },
+    revalidate: 60,
   };
 }
 
--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,7 @@ export function getStaticProps() {
     props: {
       projects: randomizePosts,
     },
+    revalidate: 60,
   };
 }
 
