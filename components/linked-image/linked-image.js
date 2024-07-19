2bba25c6bbf7445a0dc04d61901881ba6f35e2e1
Aviv Faraj <avivfaraj4@gmail.com>
2024-07-18 20:38:41 -0400

    resize honors images

--- components/linked-image/linked-image.js
+++ components/linked-image/linked-image.js
@@ -3,23 +3,21 @@ import Image from "next/legacy/image";
 import Link from "next/link";
 
 function LinkedImage(props) {
-  const { src, height, width, alt } = props;
+    const { src, height, width, alt } = props;
 
-  console.log(src);
-  return (
-    <Link href={src} target="_blank">
-      {/* Note: NextJS has deprecated <a> within Link, but
-                      target="_blank" didn't work, so I had to add <a> */}
-      <Image
-        src={src}
-        // layout={"fixed"}
-        height={height}
-        width={width}
-        alt={alt}
-        loading="lazy"
-      />
-    </Link>
-  );
+    console.log(src);
+    return (
+        <Link href={src} target="_blank">
+            <Image
+                src={src}
+                // layout={"fixed"}
+                height={height}
+                width={width}
+                alt={alt}
+                loading="lazy"
+            />
+        </Link>
+    );
 }
 
 export default LinkedImage;
