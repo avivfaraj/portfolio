67138142fae197180084a7b85d649e97a7ea9cfc
Aviv Faraj <avivfaraj4@gmail.com>
2024-08-02 22:20:51 -0400

    Fixed css

--- helpers/custom-components-utils.js
+++ helpers/custom-components-utils.js
@@ -23,7 +23,7 @@ const customComponents = {
   li(props) {
     return (
       <li className={Styles.customli}>
-        <span className={Styles.licontent}>{props.children}</span>
+        {props.children}
       </li>
     );
   },
@@ -32,21 +32,25 @@ const customComponents = {
   },
   h1(props) {
       return (
-        <h1 className={Styles.h1}>
+        <div className={Styles.h1}>
+        <h1 >
           {props.children}
         </h1>
+        </div>
       );
   },
   h2(props) {
     try {
       const { idTag, header } = getID(props.children[0]);
       return (
-        <h2 id={idTag} className={Styles.h2}>
+        <div className={Styles.h2}>
+        <h2 id={idTag}>
           {header}
         </h2>
+        </div>
       );
     } catch {
-      return <h2 className={Styles.h2}>{props.children[0]}</h2>;
+      return <div  className={Styles.h2}><h2 >{props.children[0]}</h2></div>;
     }
   },
   h4(props) {
@@ -124,6 +128,7 @@ const customComponents = {
       const caption = metastring?.match(/{caption: (.*?)}/)?.pop();
 
       return (
+        <div>
         <figure className={Styles.fig}>
           <Image
             src={`/images/${image.properties.src}`}
@@ -132,12 +137,14 @@ const customComponents = {
             height={height}
             className={Styles.image}
           />
+          
           {hasCaption && (
             <>
               <figcaption className={Styles.figcap}>{caption}</figcaption>
             </>
           )}
         </figure>
+        </div>
       );
     }
 
